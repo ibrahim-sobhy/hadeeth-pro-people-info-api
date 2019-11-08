46b97eca83e19f25560581329a59b2d5a72ec13c
Ibrahim Freig <ibrahim.sobhy.cse07@gmail.com>
2019-11-08 15:43:14 +0100

    fix the deploy section
    Signed-off-by: Ibrahim Freig <ibrahim.sobhy.cse07@gmail.com>

---
+++ docker_push.sh
@@ -0,0 +1,3 @@
+docker build -t ibrahimsobhy/hadeeth-pro-people-info:latest -t ibrahimsobhy/hadeeth-pro-people-info:$SHA .
+docker push ibrahimsobhy/hadeeth-pro-people-info:latest
+docker push ibrahimsobhy/hadeeth-pro-people-info:$SHA
